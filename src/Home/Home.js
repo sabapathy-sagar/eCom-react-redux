25d11d0c7245b8eee6f80d1b3cc1478b2b476665
Sagar Rathna Sabapathy <33907389+sabapathy-sagar@users.noreply.github.com>
2018-06-24 20:32:13 +0200

    feat(setup welcome componet)

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,3 +1,19 @@
 import React from "react";
+import Welcome from "../Welcome/Welcome";
+import { Link } from "react-router-dom";
 
-export default () => <div>Home</div>;
+export default () => (
+  <div class="hero-body has-background-grey-lighter">
+    <div class="container has-text-centered">
+      <div class="title">
+        <Welcome name="DCP" />
+      </div>
+      <h2 class="subtitle">
+        Take a look at our products:{" "}
+        <Link to="/products">
+          <button class="button is-primary is-rounded">Go To Products</button>
+        </Link>
+      </h2>
+    </div>
+  </div>
+);
