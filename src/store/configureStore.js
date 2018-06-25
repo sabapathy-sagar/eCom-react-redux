13d3ea1a62ac4700833af5c8652ed88d544f8854
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-06-25 17:35:53 +0200

    feat(enable redux dev tools)

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import dcpReducer from "../reducers/dcpReducer";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
-  const store = createStore(dcpReducer);
+  const store = createStore(
+    dcpReducer,
+    composeEnhancers()
+  );
   return store;
 };
